9a1e699c384d56cbe38dddeeca6651a6a7194e78
keusinis <220072052+keusinis@users.noreply.github.com>
2026-03-12 11:17:08 +0200

    Plug librarian to cli

--- bin/main.rb
+++ bin/main.rb
@@ -3,9 +3,14 @@
 require 'csv'
 require_relative '../lib/cli'
 require_relative '../lib/bouncer'
+require_relative '../lib/librarian'
 
 users_file_path = File.join(File.dirname(__FILE__), '..', 'data', 'users.db')
+books_file_path = File.join(File.dirname(__FILE__), '..', 'data', 'books.csv')
+borrowed_file_path = File.join(File.dirname(__FILE__), '..', 'data', 'borrowed_books.db')
+
+librarian = Librarian.new(books_file_path, borrowed_file_path)
 bouncer = Bouncer.new(users_file_path)
-cli = CLI.new(nil, bouncer)
+cli = CLI.new(librarian, bouncer)
 
 cli.start
