dad8320920dbf6b352688a32bc4d849c0cc3658f
keusinis <220072052+keusinis@users.noreply.github.com>
2026-03-12 10:53:56 +0200

    Add librarian functionality: list available books

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
 class Librarian
-  def print_books
-    all_books_table = CSV.parse(File.read('./data/books.csv'),
-                                headers: true)
-    all_books_table.by_col[2].each do |title|
-      puts title
+  def initialize(books_file_path, borrowed_file_path)
+    @books_file_path = books_file_path
+    @borrowed_file_path = borrowed_file_path
+    @books = load_books
+    @borrowed_books = load_borrowed_books
+  end
+
+  def load_books
+    CSV.read(@books_file_path, headers: true)
+  end
+
+  def load_borrowed_books
+    return [] unless File.exist?(@borrowed_file_path)
+
+    File.readlines(@borrowed_file_path).map(&:chomp)
+  end
+
+  def list_available_books
+    puts @borrowed_books.count
+    puts 'Book ID | Book Name | Author | Release Year'
+    @books.each do |book|
+      next if @borrowed_books.include?(book['Book ID'])
+
+      puts book.fields.join(' | ')
     end
   end
 end
